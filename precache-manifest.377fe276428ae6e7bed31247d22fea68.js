5defe0acbb53351e2b1703b2c24f6457d7118ac7
unknown <yrobert.aleks.zuk@gmail.com>
2022-02-06 09:58:05 +0100

    Updates

---
+++ precache-manifest.377fe276428ae6e7bed31247d22fea68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49de5049fda1cfb6ed2f95da84094512",
+    "url": "/nasa-app/index.html"
+  },
+  {
+    "revision": "e519847e554618cf8726",
+    "url": "/nasa-app/static/css/main.660b55fb.chunk.css"
+  },
+  {
+    "revision": "c192a5cc67995528c38a",
+    "url": "/nasa-app/static/js/2.b098b4c3.chunk.js"
+  },
+  {
+    "revision": "9476677600d6a88b16f87b72b2f26b83",
+    "url": "/nasa-app/static/js/2.b098b4c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e519847e554618cf8726",
+    "url": "/nasa-app/static/js/main.fa9f58ba.chunk.js"
+  },
+  {
+    "revision": "c3970e2f9a98ce78f24e",
+    "url": "/nasa-app/static/js/runtime-main.1c4d3ad0.js"
+  }
+]);
